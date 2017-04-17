77da08b845f5c444e1b822f2d82d7a36d3b0a6e5
Roderick <notmine@rjbradley.local>
2017-04-17 17:21:15 -0500

    Admin Dashboard is now useable

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+resources :posts
+resources :topics
+resources :admin_users
+
+    root to: "users#index"
+  end
+
   resources :topics do
     scope module: :topics do
       resources :posts, except: [:new, :create]
