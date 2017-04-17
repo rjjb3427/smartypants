446cc2439ff0495841f18132db8216ee355a3184
Roderick <notmine@rjbradley.local>
2017-04-17 15:56:26 -0500

    fixed the role error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  after_initialize :set_defaults
 
   validates_presence_of :first_name, :last_name
   validates :username, uniqueness: true, presence: true, format: { with: /\A[a-zA-Z]+([a-zA-Z]|\d)*\Z/ }
@@ -13,11 +12,5 @@ class User < ActiveRecord::Base
 
   def full_name
     self.first_name + " " + self.last_name
-  end
-
-  private
-
-  def set_defaults
-    self.role ||= 'student'
   end
 end
